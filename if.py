38b4433e60d1d46ad83680074ec5a85d5806a917
João Pedro Chaves <joaopedrochavescontato@gmail.com>
2026-06-09 00:35:07 -0300

    docs: add explanatory comments to if.py script

--- if.py
+++ if.py
@@ -1,10 +1,17 @@
+# If e Else / Se e Senão
+
+#definição de variáveis input
 valor_trabalho = float(input("Digite o valor do trabalho: "))
 horas_trabalhadas = float(input("Digite as horas trabalhadas: "))
 
+# Tomada de decisão (decisão baseada no valor da variável horas_trabalhadas)
 if (horas_trabalhadas >= 100):
     bonus = 500.00
 else:
     bonus = 0
 
+# Soma o valor do trabalho com as horas trabalhadas e soma o bônus
 salario = valor_trabalho * horas_trabalhadas + bonus
+
+# Exibição do resultado no formato moeda brasileira
 print(f"O salario total é de R${salario}")
